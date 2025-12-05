0503200a5b4e3c7bd1e919a9beab697bcd15675b
guptakaran-01 <guptakaranmanoj@amityonline.com>
2025-12-05 12:25:52 +0530

    Command Design Pattern

--- Command_design_pattern.cpp
+++ Command_design_pattern.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Icommand
@@ -21,6 +22,23 @@ public:
         cout << "Light is OFF" << endl;
     }
 };
+
+class fan 
+{
+public:
+    void on()
+    {
+        cout << "Fan is ON" << endl;
+    }
+    void off()
+    {
+        cout << "Fan is OFF" << endl;
+    }
+};
+
+
+
+
 
 class LightCommand : public Icommand
 {
@@ -45,44 +63,89 @@ public:
     }
 };
 
+class FanCommand : public Icommand{
+    fan* f;
+    public:
+    FanCommand(fan* f){
+        this->f=f;
+    }
+
+    void execute(){
+        f->on();
+    }
+    void undo(){
+        f->off();
+    }
+    ~FanCommand(){}
+};
+
 class Remote
 {
-    Icommand *com;
-    bool val;
+    static const int button=4;
+    Icommand *com[button];
+    bool val[button];
 
 public:
-    Remote(Icommand *command)
+    Remote()
     {
-        this->com = command;
-        val = false;
+        for(int i=0 ; i<button ; i++){
+            com[i]=nullptr;
+            val[i]=false;
+        }
     }
-    void perform()
+
+    void setcommand(Icommand* cmd , int index){
+        if(index < button && index>=0){
+            if(com[index]!=nullptr){
+                delete com[index];
+            }
+                com[index]=cmd;
+                val[index]=false ;
+            
+        }
+    }
+
+    void perform(int index)
     {
-        if (val == false)
-        {
-            com->execute();
-        }
-        else
-        {
-            com->undo();
-        }
-        val = !val;
+           if(index <= button && index>=0 && com[index]!=nullptr){
+            if(val[index]==false){
+                com[index]->execute();
+            }else{
+                com[index]->undo();
+            }
+            val[index]=!val[index];
+           }
+           else{
+            cout<<"No command is assigned to the index"<<endl;
+           }
     }
 
     ~Remote()
     {
-        delete (com);
+        for(int i=0 ; i<button ; i++){
+            delete com[i];
+        }
     }
 };
 
 int main()
 {
     Light *l1 = new Light;
-    Icommand *Lee = new LightCommand(l1);
+    fan* f1=new fan;
 
-    Remote *rem = new Remote(Lee);
-    rem->perform();
-    rem->perform();
+    Remote *rem = new Remote();
+    rem->setcommand((new LightCommand(l1)),0);
+    rem->setcommand((new FanCommand(f1)),1);
+    
+    rem->perform(0);
+    rem->perform(1);
+
+    rem->perform(0);
+    rem->perform(1);
+
+    rem->perform(2);
+
+
 
     delete rem;
     delete l1;
